b1a11780e2c74ad744ac633ee8b3ac3ccc3aeee9
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-09-06 16:30:45 +0530

    Add files via upload

---
+++ 12 JavaScript/tweetCount.js
@@ -0,0 +1,7 @@
+var name = "mayank";
+name.length;
+
+var tweet = prompt("write a tweet!! (only 140 Character is allow to write..)");
+var tweetCount = tweet.length;
+alert("you written " + tweetCount + " Character " + "and remaning Character is " + (140-tweetCount) );
+
