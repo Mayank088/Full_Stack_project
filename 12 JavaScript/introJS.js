5c41289ac605c1c8d3769b67d987fe0959fb1f5f
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-09-06 16:20:38 +0530

    Add files via upload

---
+++ 12 JavaScript/introJS.js
@@ -0,0 +1,13 @@
+alert("hello");
+
+prompt("my naem is....???");
+
+var myName = "Mayank"
+
+myName = "mr. Parmar"
+
+alert(myName);
+
+var yourName = prompt("what is your name??");
+
+alert(yourName);
