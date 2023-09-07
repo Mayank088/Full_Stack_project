730123b5bf868bd7ba0d7b4917f9f815165675bb
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-09-07 09:22:24 +0530

    Add files via upload

---
+++ 12 JavaScript/function_parameter.js
@@ -0,0 +1,51 @@
+function getMilk(bottle) { 
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("buy " + bottle + " bottle of milk")
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(2);
+
+function getMilk2(money) { 
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+
+    var numberOfBottle = Math.floor(money / 1.5);
+    console.log("buy " + numberOfBottle + " bottle of milk")
+
+    
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk2(5);
