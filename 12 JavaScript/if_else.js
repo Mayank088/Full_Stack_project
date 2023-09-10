9eb7bef7576cce1243354bf6d034f15f6ca9b80c
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-09-10 10:15:30 +0530

    Add files via upload

---
+++ 12 JavaScript/if_else.js
@@ -0,0 +1,19 @@
+var a = 1;
+var b = "1";
+
+if (a === b) {                     //=== it care about data type so ans is no
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
+
+var c = 2;
+var d= "2";
+
+if (c == d) {             // == is does't care about data type so ans is yes 
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
