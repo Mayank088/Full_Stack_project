4f58183572122edd932fe7dd64230c9f1517e558
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-09-10 10:54:32 +0530

    Add files via upload

---
+++ 12 JavaScript/advance_BMI.js
@@ -0,0 +1,26 @@
+function bmiCalculator (weight, height) {
+
+    var interpretation = (weight / Math.pow (height,2));
+
+    if (interpretation <= 18.5) {
+
+        return  "Your BMI is " + interpretation + ", so you are underweight." ;
+
+    }
+
+    else if (interpretation > 18.5 && interpretation <= 24.9){
+
+        return  "Your BMI is " + interpretation + ", so you have a normal weight." ;
+
+    }
+
+    else if (interpretation > 24.9){
+
+        return  "Your BMI is " + interpretation + ", so you are overweight." ;
+
+    }
+
+    return interpretation;
+}
+
+console.log(bmiCalculator(55,13));
