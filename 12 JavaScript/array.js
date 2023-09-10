53948d92baedd2424e558660488fbef775a1b0c3
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-09-10 12:21:44 +0530

    Add files via upload

---
+++ 12 JavaScript/array.js
@@ -0,0 +1,8 @@
+var gaustList = ["mayank" , "bharat" , "milan" , "arpita"];
+var gaustName = prompt("what is your name??");
+if (gaustList.includes(gaustName)) {
+    alert("Welcome");
+}
+else{
+    alert("sorry, maybe next time.");
+}
